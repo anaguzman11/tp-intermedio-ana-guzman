b6300c596960461db933485921d179ee8629689c
anaguzman11 <aguzica@gmail.com>
2026-01-31 01:01:23 -0300

    primer commit

---
+++ backend/src/config/db.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/booksdb");
+    console.log("MongoDB conectado 🚀");
+  } catch (error) {
+    console.error("Error al conectar MongoDB:", error);
+    process.exit(1);
+  }
+};
